ed233bd36799c598e4608c4b67cb9db19ca84a0e
mrschmiklz <33757454+mrschmiklz@users.noreply.github.com>
2024-05-08 18:48:11 -0600

    Update setup_and_run.cmd

--- setup_and_run.cmd
+++ setup_and_run.cmd
@@ -14,9 +14,17 @@ call venv-imagegen\Scripts\activate
 echo [INFO] Installing required packages from requirements.txt
 pip install -r requirements.txt
 
-:: Print message about running the Python script
-echo [INFO] Running configure_and_run.py
-python configure_and_run.py
+:: Print message about running the Python GUI script
+echo [INFO] Running configure_and_run_gui.py
+python configure_and_run_gui.py
+
+:: Check for errors
+if %errorlevel% neq 0 (
+    echo [ERROR] Error occurred while running configure_and_run_gui.py
+    echo [ERROR] Check the terminal output for details.
+    pause
+    goto :END
+)
 
 :: Deactivate the virtual environment
 echo [INFO] Deactivating virtual environment
@@ -24,4 +32,6 @@ deactivate
 
 :: Print completion message
 echo [INFO] Setup and run process completed.
+
+:END
 pause
